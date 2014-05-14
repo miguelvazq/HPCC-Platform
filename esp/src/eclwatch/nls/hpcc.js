fd6e63f425a485d329bd69f1a7d12e7e9526ffef
Miguel Vazquez <miguel.vazquez@lexisnexis.com>
2014-05-13 21:48:40 -0400

    HPCC-10943 Add overwrite option when uploading the same file
    Fixes HPCC-10943
    
    Signed-off by: Miguel Vazquez <miguel.vazquez@lexisnexis.com>
    
    Signed-off-by: Miguel Vazquez <miguel.vazquez@lexisnexis.com>

--- esp/src/eclwatch/nls/hpcc.js
+++ esp/src/eclwatch/nls/hpcc.js
@@ -273,6 +273,7 @@ define({root:
     Outputs: "Outputs",
     Overview: "Overview",
     Overwrite: "Overwrite",
+    OverwriteMessage: "Some file(s) already exist. Please check override box to continue.",
     Owner: "Owner",
     PackageContent: "Package Content",
     PackageContentNotSet: "Package content not set",
