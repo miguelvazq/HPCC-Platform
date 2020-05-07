d2a35071963f81d59ddf1ccde05ebcf3bd16c395
Gordon Smith <GordonJSmith@gmail.com>
2020-05-07 09:14:57 +0100

    Make DojoComponent re-entrant
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- esp/src/eclwatch/ActivityWidget.js
+++ esp/src/eclwatch/ActivityWidget.js
@@ -230,6 +230,11 @@ define([
             promises.refresh();
         },
 
+        destroy: function (args) {
+            this.inherited(arguments);
+            this._diskUsage.target(null);
+        },
+
         postCreate: function (args) {
             this.inherited(arguments);
             var context = this;
--- esp/src/src/react/dojoComponent.tsx
+++ esp/src/src/react/dojoComponent.tsx
@@ -1,15 +1,18 @@
+import * as registry from "dijit/registry";
 import * as React from "react";
+import * as ReactDOM from "react-dom";
 import { resolve } from "../Utility";
 
 export interface DojoProps {
-    widget: string;
+    widgetClassID: string;
     params?: object;
     onWidgetMount?: (widget) => void;
 }
 
 export interface DojoState {
-    placeholderID: string;
-    widgetID: string;
+    uid: number;
+    widgetClassID: string;
+    widget: any;
 }
 
 let g_id = 0;
@@ -17,21 +20,27 @@ export class DojoComponent extends React.Component<DojoProps, DojoState> {
 
     constructor(props: DojoProps) {
         super(props);
-        const id = ++g_id;
         this.state = {
-            placeholderID: `dojo-component-${id}`,
-            widgetID: `dojo-component-widget-${id}`
+            uid: ++g_id,
+            widgetClassID: "",
+            widget: null
         };
     }
 
-    shouldComponentUpdate(nextProps: Readonly<DojoProps>, nextState: Readonly<DojoState>): boolean {
-        return false;
-    }
+    dojoLoad(widgetClassID: string) {
+        this.setState({ widgetClassID });
 
-    componentDidMount() {
-        resolve(this.props.widget, WidgetClass => {
+        const domNode: HTMLElement = ReactDOM.findDOMNode(this);
+        domNode.innerText = "";
+
+        const elem = document.createElement("div");
+        domNode.appendChild(elem);
+
+        resolve(widgetClassID, WidgetClass => {
+            const uid = ++g_id; // Incrementing the ID should not really be needed  ---
+
             const widget = new WidgetClass({
-                id: this.state.widgetID,
+                id: `dojo-component-widget-${uid}`,
                 style: {
                     margin: "0px",
                     padding: "0px",
@@ -39,20 +48,54 @@ export class DojoComponent extends React.Component<DojoProps, DojoState> {
                     height: "100%"
                 }
             });
-            widget.placeAt(this.state.placeholderID, "replace");
+            widget.placeAt(elem, "replace");
             widget.startup();
             widget.resize();
             if (widget.init) {
                 widget.init(this.props.params || {});
             }
+
+            this.setState({ uid, widget });
             if (this.props.onWidgetMount) {
                 this.props.onWidgetMount(widget);
             }
         });
     }
 
+    dojoUnload(nextWidgetClassID: string) {
+        if (this.state.widget) {
+            this.state.widget.destroyRecursive(true);
+
+            //  Should not be needed  ---
+            registry.toArray().filter(w => w.id.indexOf(`dojo-component-widget-${this.state.uid}`) === 0).forEach(w => {
+                w.destroyRecursive(true);
+            });
+            //  ---
+
+            const domNode: HTMLElement = ReactDOM.findDOMNode(this);
+            domNode.innerHTML = `...loading ${nextWidgetClassID}...`;
+            this.setState({ widget: null });
+        }
+    }
+
+    shouldComponentUpdate(nextProps: Readonly<DojoProps>, nextState: Readonly<DojoState>): boolean {
+        if (this.props.widgetClassID !== nextProps.widgetClassID) {
+            this.dojoUnload(nextProps.widgetClassID);
+            this.dojoLoad(nextProps.widgetClassID);
+        }
+        return false;
+    }
+
+    componentDidMount() {
+        this.dojoLoad(this.props.widgetClassID);
+    }
+
+    componentWillUnmount() {
+        this.dojoUnload(this.props.widgetClassID);
+    }
+
     render() {
-        return <div id={this.state.placeholderID}>...loading {this.props.widget}...</div>;
+        return <div style={{ width: "100%", height: "100%" }}>...loading {this.props.widgetClassID}...</div>;
     }
 
 }
