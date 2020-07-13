f62c7ccfc1231bba38cce1f9b3742c66ace4d359
Gordon Smith <GordonJSmith@gmail.com>
2020-07-13 15:02:49 +0100

    chore(merge): Add missing file
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

---
+++ esp/src/src/react/hooks/userContext.ts
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export const UserAccountContext = React.createContext(null);
+export const UserNotificationCount = React.createContext(0); //TODO
+export const FavoritePages = React.createContext([]); //TODO
